17aa48343570fb611f0b482d3cee36821e97734e
VerstandTsai <hackertsaigenz@gmail.com>
2022-04-03 22:40:30 +0800

    Now use bot.commands instead of on_message

--- hbot.py
+++ hbot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -8,15 +9,15 @@ from shutil import rmtree
 from threading import Thread
 import asyncio
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'The bot has logged in as {client.user}')
+    print(f'The bot has logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if len(message.content) == 6 and message.content.isdigit():
@@ -26,48 +27,50 @@ async def on_message(message):
         title = soup.find('h2', class_='title').find('span', class_='pretty').text
         await message.channel.send(f'標題：{title}\n網址：{url}')
 
-    if message.content.startswith('!geth'):
-        num = message.content.split()[1]
-        download_folder = './downloads'
-        if len(os.listdir(download_folder)) > 5:
-            rmtree(download_folder)
-            os.mkdir(download_folder)
+    await bot.process_commands(message)
 
-        url = f'https://nhentai.net/g/{num}/'
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        title = soup.find('h2', class_='title').find('span', class_='pretty').text
-        pages = int(soup.find(id='tags').find_all(class_='name')[-1].text)
+@bot.command()
+async def geth(ctx, arg):
+    download_folder = './downloads'
+    if len(os.listdir(download_folder)) > 5:
+        rmtree(download_folder)
+        os.mkdir(download_folder)
 
-        await message.channel.send(f'正在下載：{title}')
-        progress = await message.channel.send(f'進度：0/{pages}')
+    url = f'https://nhentai.net/g/{arg}/'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    title = soup.find('h2', class_='title').find('span', class_='pretty').text
+    pages = int(soup.find(id='tags').find_all(class_='name')[-1].text)
 
-        download_id = token_urlsafe(6)
-        download_path = f'{download_folder}/{download_id}'
-        os.mkdir(download_path)
-        def get_page(page_num):
-            page_url = f'{url}{page_num}/'
-            page_res = requests.get(page_url)
-            page_soup = BeautifulSoup(page_res.text, 'html.parser')
-            img_url = page_soup.find(id='image-container').find('img')['src']
-            img_res = requests.get(img_url)
-            open(f'{download_path}/{page_num}.jpg', 'wb').write(img_res.content)
+    await ctx.send(f'正在下載：{title}')
+    progress = await ctx.send(f'進度：0/{pages}')
 
-        threads = []
-        for i in range(pages):
-            threads.append(Thread(target=get_page, args=(i+1,)))
-            threads[i].start()
-            await asyncio.sleep(0.2)
-            await progress.edit(content=f'進度：{i+1}/{pages}')
-        for i in range(pages):
-            threads[i].join()
+    download_id = token_urlsafe(6)
+    download_path = f'{download_folder}/{download_id}'
+    os.mkdir(download_path)
+    def get_page(page_num):
+        page_url = f'{url}{page_num}/'
+        page_res = requests.get(page_url)
+        page_soup = BeautifulSoup(page_res.text, 'html.parser')
+        img_url = page_soup.find(id='image-container').find('img')['src']
+        img_res = requests.get(img_url)
+        open(f'{download_path}/{page_num}.jpg', 'wb').write(img_res.content)
 
-        with ZipFile(f'{download_path}.zip', 'w') as zf:
-            for i in range(1, pages+1):
-                zf.write(f'{download_path}/{i}.jpg', f'{i}.jpg')
-        rmtree(f'{download_path}')
+    threads = []
+    for i in range(pages):
+        threads.append(Thread(target=get_page, args=(i+1,)))
+        threads[i].start()
+        await asyncio.sleep(0.1)
+        await progress.edit(content=f'進度：{i+1}/{pages}')
+    for i in range(pages):
+        threads[i].join()
 
-        await progress.edit(content='下載完成，點擊以下連結以下載')
-        await message.channel.send(f'https://hentaibot-discord.herokuapp.com/downloads/{download_id}')
+    with ZipFile(f'{download_path}.zip', 'w') as zf:
+        for i in range(1, pages+1):
+            zf.write(f'{download_path}/{i}.jpg', f'{i}.jpg')
+    rmtree(f'{download_path}')
 
-client.run(os.getenv('TOKEN'))
+    await progress.edit(content='下載完成，點擊以下連結以下載')
+    await ctx.send(f'https://hentaibot-discord.herokuapp.com/downloads/{download_id}')
+
+bot.run(os.getenv('TOKEN'))
